397bf8396f69975ef13d0804b6a40dff754f6bf6
Gautamabhi02 <gautamabhi6517@gmail.com>
2025-08-06 14:34:16 -0400

    remove comment in main page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart'; // Add this import
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/services.dart';
 import 'package:rekloze/screens/EditContractTimelinePdfPage.dart';
 import 'package:rekloze/screens/calendar_page.dart';
@@ -21,7 +21,7 @@ void main() async {
   }
 
   runApp(
-    ProviderScope( // Wrap with ProviderScope
+    ProviderScope(
       child: MyApp(initialRoute: (token != null) ? '/contract' : '/loginPage'),
     ),
   );
@@ -32,7 +32,7 @@ class MyApp extends ConsumerWidget { // Changed to ConsumerWidget
   const MyApp({super.key, required this.initialRoute});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) { // Added WidgetRef
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Rekloze App',
       debugShowCheckedModeBanner: false,
@@ -45,7 +45,7 @@ class MyApp extends ConsumerWidget { // Changed to ConsumerWidget
           child: child!,
         );
       },
-      initialRoute: initialRoute, // Use the passed initialRoute
+      initialRoute: initialRoute,
       routes: {
         '/loginPage': (context) => const LoginPage(),
         '/homePage': (context) => const HomePage(),
@@ -78,31 +78,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
